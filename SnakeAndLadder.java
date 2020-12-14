5c5656dcd12039e6507a60af774f57257ae296c4
ashwini-8 <ashwinip309@gmail.com>
2020-12-14 19:39:39 +0530

    Added: UC1 Snake And Ladder With Single Player

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -1,6 +1,8 @@
 package com.java.project;
 
 public class SnakeAndLadder {
+	 int START_POSITION = 0;
+	 int PLAYER = 1;
 	
 	public static void main(String[] args) {
 		System.out.println("Welcome to Snake and Ladder Game");
