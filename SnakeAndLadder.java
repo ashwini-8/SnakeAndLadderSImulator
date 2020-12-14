fe4096811e4a76c12787c4ac7412b988d10bc226
ashwini-8 <ashwinip309@gmail.com>
2020-12-14 19:51:52 +0530

    Added: UC2 Rolling a Dice with random function

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -1,10 +1,21 @@
 package com.java.project;
+import java.util.Random;
 
 public class SnakeAndLadder {
-	 int START_POSITION = 0;
-	 int PLAYER = 1;
+	// UC1 snake and ladder game played with single player at position 0
+	int START_POSITION = 0;
+	int PLAYER = 1;
+	static Random random = new Random();
 	
 	public static void main(String[] args) {
 		System.out.println("Welcome to Snake and Ladder Game");
+		int diceNum = rollingDice();
+		System.out.println("diceNum:" + diceNum);
+		
+	}
+	// UC2 rolling a dice
+	static int rollingDice() {
+		int diceNum = random.nextInt(6) + 1;
+		return diceNum;
 		}
 }
