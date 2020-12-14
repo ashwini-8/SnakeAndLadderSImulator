6aa8c7f14d01aecc0801965874fc024a14d48018
ashwini-8 <ashwinip309@gmail.com>
2020-12-14 19:19:49 +0530

    Added:Snake And Ladder Game

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -2,11 +2,7 @@ package com.java.project;
 
 public class SnakeAndLadder {
 	
-	static final int STARTPOSITION = 0;
-	static final int PLAYER = 1;
-	
 	public static void main(String[] args) {
 		System.out.println("Welcome to Snake and Ladder Game");
-		
 		}
 }
