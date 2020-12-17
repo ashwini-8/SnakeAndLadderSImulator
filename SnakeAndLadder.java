89a2389f8aa07a1dd73e70b5536365e7cee64b3e
ashwini-8 <ashwinip309@gmail.com>
2020-12-17 10:04:40 +0530

    Added:Snake and ladder game for two players

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -5,7 +5,8 @@ public class SnakeAndLadder {
 	// UC1 snake and ladder game played with single player at position 0
 	static final int START_POSITION = 0;
 	static final int END_POSITION = 100;
-	int PLAYER = 1;
+    static int playerOne = 0;
+    static int playerTwo = 0;
 	static final int LADDER = 1;
 	static final int SNAKE = 2;
 	static final int NOPLAY = 0;
@@ -13,51 +14,52 @@ public class SnakeAndLadder {
 	private static int count = 0;
 	
 	public static void main(String[] args) {
-		System.out.println("Welcome to Snake and Ladder Game");
-		checkPosition();
-		
-	}
+        System.out.println("Welcome to Snake and Ladder Game");
+        while(playerOne < END_POSITION && playerTwo < END_POSITION)
+		{
+            playerOne = checkPosition(playerOne);
+            playerTwo = checkPosition(playerTwo);
+        }
+        if(playerOne >= END_POSITION){
+            System.out.println("Player One Wins!"); 
+        }
+        else System.out.println("Player Two Wins!");
+        System.out.println("Num of times dice is tossed = "+count);
+    }
+    
 	// UC2 rolling a dice
 	static int rollingDice() {
 		int diceNum = random.nextInt(6) + 1;
 		System.out.println("DiceNum: " + diceNum);
-		//UC6 no of times a dice is tossed
 		count++;
-		System.out.println("count: " + count);
 		return diceNum;
-		}
+        }
+        
 	// UC3 no play,ladder,snake
-		static void checkPosition() {
-			//UC4 repeat till the player reaches 100
-			int currentPosition=START_POSITION;
-			while(currentPosition < END_POSITION) {
-			int diceNum = rollingDice();
-			int choice = random.nextInt(3);
-			System.out.println("choice: "+choice);
-			
-			switch (choice){
-				case NOPLAY:
-					System.out.println("no play: " + currentPosition);
-					break;
-				case LADDER:
-					//UC5 player reaches 100
-					int temp;
-					temp = currentPosition;
-					temp = temp + diceNum;
-					if (temp <= END_POSITION) {
-						currentPosition = temp;
-						System.out.println("ladder: " + currentPosition);
-					}
-					break;
-				case SNAKE:
-					currentPosition = currentPosition - diceNum;
-					if(currentPosition < START_POSITION){
-						currentPosition = START_POSITION;
-					} 
-					System.out.println("Snake: " + currentPosition);
-					break;	
-				}
-			}
-		}
-
-}
+	static int checkPosition(int currentPosition) {
+        //UC4 repeat till the player reaches 100
+        int diceNum = rollingDice();
+        int choice = random.nextInt(3);
+        System.out.println("choice: "+choice);
+        
+        switch (choice){
+            case NOPLAY:
+                System.out.println("no play: " + currentPosition);
+                break;
+            case LADDER:
+                //UC5 player reaches 100
+                currentPosition += diceNum;
+                System.out.println("ladder: " + currentPosition);
+                break;
+            case SNAKE:
+                currentPosition -= diceNum;
+                if(currentPosition < START_POSITION){
+                    currentPosition = START_POSITION;
+                } 
+                System.out.println("Snake: " + currentPosition);
+                break;	
+            }
+        return currentPosition;
+        
+        }
+    }
